8276886514d705d342ec1ae40eb1d9a687ee0158
Carlos Aguilera <18aguilerac@gmail.com>
2025-01-12 22:52:08 -0800

    update

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,38 +1,38 @@
 import { Module } from '@nestjs/common';
-import { PostsController } from 'src/controllers/posts.controller';
-import { PostsService } from 'src/services/posts.service';
+// import { PostsController } from 'src/controllers/posts.controller';
+// import { PostsService } from 'src/services/posts.service';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { PostSchema } from './schemas/post.schema';
-import { ItemSchema } from './schemas/item.schema';
-import { ItemsController } from './controllers/items.controller';
-import { ItemsService } from './services/items.service';
-import { CategorySchema } from './schemas/category.schema';
-import { CategoriesController } from './controllers/categories.controller';
-import { CategoriesService } from './services/categories.service';
-import { SendGridController } from './controllers/send-grid.controller';
-import { ApplicationSchema } from './schemas/application.schema';
-import { SendGridService } from './services/send-grid.service';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { PostSchema } from './schemas/post.schema';
+// import { ItemSchema } from './schemas/item.schema';
+// import { ItemsController } from './controllers/items.controller';
+// import { ItemsService } from './services/items.service';
+// import { CategorySchema } from './schemas/category.schema';
+// import { CategoriesController } from './controllers/categories.controller';
+// import { CategoriesService } from './services/categories.service';
+// import { SendGridController } from './controllers/send-grid.controller';
+// import { ApplicationSchema } from './schemas/application.schema';
+// import { SendGridService } from './services/send-grid.service';
 import { PingController } from './controllers/ping.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
-    MongooseModule.forFeature([
-      { name: 'post', schema: PostSchema },
-      { name: 'item', schema: ItemSchema },
-      { name: 'category', schema: CategorySchema },
-      { name: 'application', schema: ApplicationSchema },
-    ]),
+    // MongooseModule.forRoot(process.env.MONGO_URI),
+    // MongooseModule.forFeature([
+    //   { name: 'post', schema: PostSchema },
+    //   { name: 'item', schema: ItemSchema },
+    //   { name: 'category', schema: CategorySchema },
+    //   { name: 'application', schema: ApplicationSchema },
+    // ]),
   ],
   controllers: [
-    PostsController,
-    ItemsController,
-    CategoriesController,
-    SendGridController,
+    // PostsController,
+    // ItemsController,
+    // CategoriesController,
+    // SendGridController,
     PingController,
   ],
-  providers: [PostsService, ItemsService, CategoriesService, SendGridService],
+  // providers: [PostsService, ItemsService, CategoriesService, SendGridService],
 })
 export class AppModule {}
