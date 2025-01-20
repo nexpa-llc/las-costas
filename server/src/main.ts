f46ceb0c2c13b5178023b7e3c2fb8b2ed7c4e3b5
Carlos Aguilera <18aguilerac@gmail.com>
2025-01-20 12:11:56 -0800

    update

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/errors/http-exception.filter';
 
 async function bootstrap() {
@@ -8,13 +8,13 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     transform: true,
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true,
+  //   }),
+  // );
 
   await app.listen(3000);
 }
