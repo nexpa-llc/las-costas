8cffc9b5770d687307786953109eabf9c0cbacaa
Carlos Aguilera <18aguilerac@gmail.com>
2025-01-20 23:53:09 -0800

    locales

--- client/src/locales/es.js
+++ client/src/locales/es.js
@@ -6,48 +6,10 @@ export default {
     view_menu: 'Ver Menú',
     order_now: 'Ordena ahora',
   },
-  promotional: {
-    header: '¡Ordene en línea ahora!',
-    hook: 'Obtenga 10% de descuento*',
-    body: 'Para las primeras 15 personas que realicen su pedido en línea.',
-  },
   hours: {
     closed: 'Cerrado, Apertura Martes a las 3:00 PM',
     opening: 'Abriendo en San Juan {location} a las 3:00 PM',
     open: 'Abierto en San Juan {location} hasta las 11:30 PM',
-  },
-  dividers: {
-    platillo: 'Nuestros Platillos de Especialidad',
-    menu: 'Menú servido diariamente',
-    testimonial: 'Testimonios',
-    categories: 'Categorías Principales',
-    cta: '¿Que estas esperando?',
-  },
-  cta: {
-    hook: 'Conéctate con nosotros y mantente informado!',
-    info: 'Omita la espera y llámenos durante el horario comercial. ¡Quiere recibir notificaciones sobre promociones y eventos, regístrese hoy!',
-    contact: {
-      open_hours: 'Horarios Abiertos',
-      hours: 'Lunes a Domingo: 3:00 PM - 11:30 PM',
-      location: 'Ubicación',
-      contact: 'Contacto',
-    },
-  },
-  form: {
-    email: 'Correo Electrónico',
-    name: 'Nombre',
-    phone_number: 'Número de Teléfono',
-    message: 'Mensaje',
-    descriptive_message: 'Mensaje descriptivo',
-    interested_in: 'Interesado en',
-    select: {
-      selection: 'Seleccione',
-      order: 'Orden',
-      job: 'Oportunidad de Trabajo',
-      other: 'Otra Cosa',
-    },
-    submit: 'Entregar',
-    sign_up: 'Inscribirse',
   },
   link: {
     home: 'Página Principal',
@@ -62,17 +24,10 @@ export default {
     careers: 'Carreras',
     schedule: 'Lunes a Domingo: Abierto',
   },
-  blog: {
-    read_more: 'Leer Más',
-  },
   error: {
     header: 'LO SENTIMOS, ¡Página No se Encontro!',
     message:
       'Si escribió la URL directamente, asegúrese de que la ortografía sea correcta.',
     homepage: 'Volver a la Página Principal',
-  },
-  menu: {
-    meat_selection: 'Selección De Carne',
-    full_menu: 'Menú Completo',
   },
 };
